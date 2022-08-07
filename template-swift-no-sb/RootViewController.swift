992bfa5ef6e85a3015418bf8de289d31131a1517
Glenn Adams <flgadams@gmail.com>
2022-08-07 09:31:47 -0400

    sanity check v5

--- template-swift-no-sb/RootViewController.swift
+++ template-swift-no-sb/RootViewController.swift
@@ -21,27 +21,32 @@
 import UIKit
 
 class RootViewController: UIViewController {
-
+    
+    let v1 = UIView()
+    let v2 = UIView()
+    let v3 = UIView()
+    let v4 = UIView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .systemMint
         
-        let v1 = UIView()
+       // let v1 = UIView()
         v1.translatesAutoresizingMaskIntoConstraints=false
         v1.backgroundColor = .green
         self.view.addSubview(v1)
         
-        let v2 = UIView()
+      //  let v2 = UIView()
         v2.translatesAutoresizingMaskIntoConstraints=false
         v2.backgroundColor = .red
         self.view.addSubview(v2)
         
-        let v3 = UIView()
+        //let v3 = UIView()
         v3.translatesAutoresizingMaskIntoConstraints = false
         v3.backgroundColor = .purple
         self.view.addSubview(v3)
         
-        let v4 = UIView()
+        //let v4 = UIView()
         v4.backgroundColor = .blue
         v4.translatesAutoresizingMaskIntoConstraints = false
         self.view.addSubview(v4)
@@ -53,7 +58,7 @@ class RootViewController: UIViewController {
         for _ in views.dropLast() {
             let g = UILayoutGuide()
             self.view.addLayoutGuide(g)
-            guides.append( g)            
+            guides.append( g)
         }
         
         let viewsLeading = self.view.leadingAnchor
@@ -78,8 +83,16 @@ class RootViewController: UIViewController {
             g.heightAnchor.constraint(equalTo: h).isActive=true
         }
         
-        
-        
+        let v5 = UIView()
+        v5.translatesAutoresizingMaskIntoConstraints=false
+        v5.backgroundColor = .red
+        self.view.addSubview(v5)
+        NSLayoutConstraint.activate([
+            v5.topAnchor.constraint(equalTo: self.view.topAnchor),
+            v5.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+            v5.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
+            v5.heightAnchor.constraint(equalToConstant: 100)
+        ])
         
         
         
@@ -87,4 +100,33 @@ class RootViewController: UIViewController {
         
     }
 }
+/*
+ var guides = [UILayoutGuide]()
+ // one fewer guides than views
+ for _ in views.dropLast() {
+     let g = UILayoutGuide()
+     self.view.addLayoutGuide(g)
+     guides.append(g)
+ }
+ // guides leading and width are arbitrary
+ let anc = self.view.leadingAnchor
+ for g in guides {
+     g.leadingAnchor.constraint(equalTo:anc).isActive = true
+     g.widthAnchor.constraint(equalToConstant:10).isActive = true
+ }
+ // guides top to previous view
+ for (v,g) in zip(views.dropLast(), guides) {
+     v.bottomAnchor.constraint(equalTo:g.topAnchor).isActive = true
+ }
+ // guides bottom to next view
+ for (v,g) in zip(views.dropFirst(), guides) {
+     v.topAnchor.constraint(equalTo:g.bottomAnchor).isActive = true
+ }
+ // guide heights equal to each other!
+ let h = guides[0].heightAnchor
+ for g in guides.dropFirst() {
+     g.heightAnchor.constraint(equalTo:h).isActive = true
+ }
 
+ 
+ */
