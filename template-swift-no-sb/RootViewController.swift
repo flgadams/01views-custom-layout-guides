f4307323da7835a08ade513c8df54b046a99e753
Glenn Adams <flgadams@gmail.com>
2022-08-06 21:22:10 -0400

    initial commit

--- template-swift-no-sb/RootViewController.swift
+++ template-swift-no-sb/RootViewController.swift
@@ -4,14 +4,34 @@
 //
 //  Created by Glenn Adams on 7/30/22.
 // A Swift project template for UIKit apps, no storyboards
+/*
+ 
+ I’ll demonstrate. We have four views that are to remain equally distributed vertically. I constrain the left and right edges of the four views, their heights, and the top of the first view and the bottom of the last view. Now we want to set the vertical position of the two middle views such that they are always equidistant from their vertical neighbors
 
+ Neuburg, Matt. Programming iOS 13 . O'Reilly Media. Kindle Edition. 
+ */
 import UIKit
 
 class RootViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .systemRed
+        self.view.backgroundColor = .systemGray
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
     }
 }
 
