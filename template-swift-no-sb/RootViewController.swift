f2e03cf10f674b0dc002dfde32ac05870130df9a
Glenn Adams <flgadams@gmail.com>
2022-08-06 22:47:21 -0400

    first commit

--- template-swift-no-sb/RootViewController.swift
+++ template-swift-no-sb/RootViewController.swift
@@ -7,8 +7,16 @@
 /*
  
  I’ll demonstrate. We have four views that are to remain equally distributed vertically. I constrain the left and right edges of the four views, their heights, and the top of the first view and the bottom of the last view. Now we want to set the vertical position of the two middle views such that they are always equidistant from their vertical neighbors
-
- Neuburg, Matt. Programming iOS 13 . O'Reilly Media. Kindle Edition. 
+--------------
+   TOP rect
+ 
+   middle rect
+ 
+   middle rect
+ 
+   Bottom rect
+-----------------
+ Neuburg, Matt. Programming iOS 13 . O'Reilly Media. Kindle Edition.
  */
 import UIKit
 
@@ -18,8 +26,38 @@ class RootViewController: UIViewController {
         super.viewDidLoad()
         self.view.backgroundColor = .systemGray
         
+        let v1 = UIView()
+        v1.translatesAutoresizingMaskIntoConstraints=false
+        v1.backgroundColor = .green
+        self.view.addSubview(v1)
         
+        let v2 = UIView()
+        v2.translatesAutoresizingMaskIntoConstraints=false
+        v2.backgroundColor = .red
+        self.view.addSubview(v2)
         
+        let v3 = UIView()
+        v3.translatesAutoresizingMaskIntoConstraints = false
+        v3.backgroundColor = .purple
+        self.view.addSubview(v3)
+        
+        let v4 = UIView()
+        v4.backgroundColor = .blue
+        v4.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(v4)
+        
+        let views = [v1,v2,v3,v4]
+        var guides = [UILayoutGuide]()
+        // add the guides to the view
+        for _ in views.dropLast() {
+            let g = UILayoutGuide()
+            self.view.addLayoutGuide(g)
+            guides.append( g)
+            
+        }
+        
+        let anc = self.view.leadingAnchor
+        // left anchor and width are arbitrary
         
         
         
